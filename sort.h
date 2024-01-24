32fd7f90088a99cf4acf9bf982b103070b08e925
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-01-24 01:47:20 -0800

    corrected the header

--- sort.h
+++ sort.h
@@ -9,7 +9,7 @@
 #define UP 0
 #define DOWN 1
 
- 
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,9 +19,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	 const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
